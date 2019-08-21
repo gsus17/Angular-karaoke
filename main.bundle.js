93569bbb64eaebe61b27ddf2aedac9886134dd53
gsus17 <suarezmolina17@gmail.com>
2019-08-21 16:06:22 -0300

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -6985,8 +6985,12 @@ var SongsService = /** @class */ (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: true
+    production: false
 };
 
 
